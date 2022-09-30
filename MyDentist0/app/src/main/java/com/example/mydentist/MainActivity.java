174458c5b67c9dbff8b08a1887213bd83886ba54
JeongHyeon Jo <cisxo@naver.com>
2022-09-30 21:01:41 +0900

    logine_view

--- MyDentist0/app/src/main/java/com/example/mydentist/MainActivity.java
+++ MyDentist0/app/src/main/java/com/example/mydentist/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.mydentist.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btn_login;
     private Button btn_move1;
     private Button btn_move2;
     private Button btn_move3;
@@ -19,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btn_move1 = findViewById(R.id.btn_move1);
         btn_move1.setOnClickListener(new View.OnClickListener() {
